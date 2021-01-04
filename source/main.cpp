b8da36f9cefde409b1c021e6d8f112180e8e0b12
huwdp <huw008@gmail.com>
2021-01-04 00:30:22 +0000

    Changes

--- source/main.cpp
+++ source/main.cpp
@@ -24,63 +24,21 @@
 #include "tokens/filetokenmanager.h"
 #include "huwinterpreter.h"
 
-void printHelp(int argc, char* argv[])
-{
-    if (argc > 0)
-    {
-        std::cerr << "1. Usage: " << argv[0] << " [filename] to run file." << std::endl;
-        std::cerr << "2. Usage: " << argv[0] << " -t [text] to run text." << std::endl;
-        std::cerr << "3. Usage: " << argv[0] << " --hc [filename] for HuwCode formatter" << std::endl;
-        std::cerr << "4. Usage: " << argv[0] << " -h or --help for help." << std::endl;
-    }
-    else
-    {
-        std::cerr << "1. Usage: [filename] to run file." << std::endl;
-        std::cerr << "2. Usage: -t [text] to run text." << std::endl;
-        std::cerr << "3. Usage: --hc for HuwCode formatter" << std::endl;
-        std::cerr << "4. Usage: -h or --help for help." << std::endl;
-    }
-}
-
 int main(int argc, char* argv[])
 {
     std::shared_ptr<HuwInterpreter::Interpreter> interpreter = std::make_shared<HuwInterpreter::Interpreter>();
-    if (argc < 2)
-    {
-        printHelp(argc, argv);
-        return 1;
-    }
-    std::string argument = argv[1];
 
-    // Optional arguments
-    bool benchmark = false;
-
-    for (int i = 0; i < argc; i++)
+    while (true)
     {
-        std::string arg= argv[i];
-        if (arg == "-b")
+        std::cout << ">>> ";
+        std::string text;
+        std::cin >> text;
+        if (text == "exit")
         {
-            benchmark = true;
-        }
-    }
-
-    if (argument == "-h" || argument == "--help")
-    {
-        printHelp(argc, argv);
-    }
-
-    else if (argument == "--hc" && argc == 3)
-    {
-        std::cout << interpreter->toString(interpreter->parseFile(argv[2])) << std::endl;
-    }
-    else
-    {
-        if (benchmark)
-        {
-            interpreter->benchmark(interpreter->parseFile(argv[1]));
             std::exit(0);
         }
-        interpreter->executeFile(argument);
+        std::cout << std::endl;
+        interpreter->execute(interpreter->parseText(text));
     }
     std::exit(0);
 }
